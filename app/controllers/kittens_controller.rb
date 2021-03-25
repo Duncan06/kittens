058e2d9a9d54b5088582ffd6ca8d377eb8c334d3
Rumsauce <tylerdownie0@gmail.com>
2021-03-25 13:02:14 -0700

    Enable json calls, adjust spacing on controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,53 +2,62 @@ class KittensController < ApplicationController
     before_action :find_kitten, only: [:show, :edit, :update, :destroy]
 
     def index
-        @kittens = Kitten.all
+      @kittens = Kitten.all
+
+      respond_to do |format|
+        format.html
+        format.json { render json: @kittens }
+      end
     end
 
     def show
+      respond_to do |format|
+        format.html
+        format.json { render json: @kitten }
+      end
     end
     
     def new
-        @kitten = Kitten.new
+      @kitten = Kitten.new
     end
 
     def create
-        @kitten = Kitten.new(kitten_params)
-        if @kitten.save
-            flash[:notice] = "Kitten saved."
-            redirect_to kitten_path(@kitten)
-        else
-            flash[:alert] = "An error occurred."
-            render :new
-        end
+      @kitten = Kitten.new(kitten_params)
+      if @kitten.save
+        flash[:notice] = "Kitten saved."
+        redirect_to kitten_path(@kitten)
+      else
+        flash[:alert] = "An error occurred."
+        render :new
+      end
     end
 
     def edit
     end
 
     def update
-        if @kitten.update(kitten_params)
-            flash[:notice] = "Kitten updated."
-            redirect_to root_path
-        else
-            flash[:alert] = "Unable to update."
-            render :edit
-        end
+      if @kitten.update(kitten_params)
+        flash[:notice] = "Kitten updated."
+        redirect_to root_path
+      else
+        flash[:alert] = "Unable to update."
+        render :edit
+      end
     end
 
     def destroy
-        @kitten.destroy
-        flash[:notice] = "Kitten deleted."
-        redirect_to root_path
+      @kitten.destroy
+      flash[:notice] = "Kitten deleted."
+      redirect_to root_path
     end
 
     private
 
-        def kitten_params
-            params.require(:kitten).permit(:name, :age, :cuteness, :softness)
-        end
+      def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+      end
 
-        def find_kitten
-            @kitten = Kitten.find(params[:id])
-        end
+      def find_kitten
+        @kitten = Kitten.find(params[:id])
+      end
 end
